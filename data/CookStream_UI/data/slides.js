cd5948ef9c2a35e8a1fdee9d0284aa5db81335bf
GrinchakAndrew <andrewgrinchak@gmail.com>
2017-03-02 18:25:48 +0200

    init

---
+++ data/CookStream_UI/data/slides.js
@@ -0,0 +1,14 @@
+{
+	"collection" : ["data/CookStream_UI/data/slide1.html",
+					"data/CookStream_UI/data/slide2.html", 
+					"data/CookStream_UI/data/slide3.html", 
+					"data/CookStream_UI/data/slide4.html", 
+					"data/CookStream_UI/data/slide5.html", 
+					"data/CookStream_UI/data/slide6.html", 
+					"data/CookStream_UI/data/slide7.html", 
+					"data/CookStream_UI/data/slide8.html",
+					"data/CookStream_UI/data/slide9.html",
+					"data/CookStream_UI/data/slide10.html",
+					"data/CookStream_UI/data/slide11.html"
+					]
+}
---
+++ data/tasks.js
@@ -0,0 +1,20 @@
+{
+	"(1) CookStream Brief " : [
+		{"General Functionality -> " : [
+					{"Purpose" : {"(1) General Purpose of the CookStream Application" : "data/CookStream_General_Purpose_Description.txt"}},
+					{"Roles" : {"(1) Roles": "data/CookStream_Roles_Description.txt"}}, 
+					{"Section #1" : {"(1) Section # 1": "data/CookStream_Section_One_Description.txt"}}, 
+					{"Section #2" : {"(1) Section # 2": "data/CookStream_Section_Two_Description.txt"}}, 
+					{"Section #3" : {"(1) Section # 3": "data/CookStream_Section_Three_Description.txt"}}
+				  ]
+		},
+		
+		{"User-Scenarios" : [
+					{"User-Scenarios by UI" : {"(1) UI Description" : "data/CookStream_UI/UI.html"}}
+				  ]
+		}
+	]
+}
+
+
+
